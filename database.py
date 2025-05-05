3665a9aac8b6dd2e09afbcc45855d294423393dd
kamlesh723 <ky2673689@gmail.com>
2025-05-05 12:54:02 +0530

    registration feature

---
+++ database.py
@@ -0,0 +1,13 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from crud import DATABASE_URL
+
+engine = create_engine(DATABASE_URL)
+
+Base = declarative_base()
+
+SessionLocal = sessionmaker(bind=engine, expire_on_commit=False)
+
+
+
